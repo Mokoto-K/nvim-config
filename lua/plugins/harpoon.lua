173d05c33236612f54b9ce12d841c08d54a674d4
mokotok <mokotok@protonmail.com>
2025-01-07 04:10:06 +0000

    Changed harpoon remapping

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,10 +10,10 @@ return {
         vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle Harpoon Menu" })  -- Toggle Harpoon menu
 
         -- Harpoon navigation keymaps
-        vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, { desc = "Navigate to file 1" })
-        vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end, { desc = "Navigate to file 1" })
-        vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end, { desc = "Navigate to file 1" })
-        vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end, { desc = "Navigate to file 1" })
+        vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end, { desc = "Navigate to file 1" })
+        vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end, { desc = "Navigate to file 2" })
+        vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end, { desc = "Navigate to file 3" })
+        vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end, { desc = "Navigate to file 4" })
     end,
 }
 
